2b4678bd5c0e0a8c3c7aa9b56dd556bbfe691837
Laura Gavazzi <160546767+LauGav07@users.noreply.github.com>
2026-01-30 12:43:09 +0100

    Add files via upload

---
+++ Quinta/server.php
@@ -0,0 +1,14 @@
+<?php
+function cmToInch($cm) {
+    return $cm / 2.54;
+}
+
+function inchToCm($inch) {
+    return $inch * 2.54;
+}
+
+$server = new SoapServer("convert.wsdl");
+$server->addFunction("cmToInch");
+$server->addFunction("inchToCm");
+$server->handle();
+?>
